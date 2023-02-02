7c16cab3bbb72dfd1e00563f5b462a8396caf834
fikrianggara <fikriseptriananggara@gmail.com>
2023-02-02 15:47:59 +0700

    change query string literal to read a sql query

--- pipeline.py
+++ pipeline.py
@@ -18,8 +18,11 @@ def fromSQLServerToCSV():
 
     # Extract from sql server db
     extractor = e.Extract()
-    query = "select top(3) * from Regsosek22.dbo.tab_view_k where [r407] <= 4 AND [r427] IS NULL"
-    queryResult, columns = extractor.FromDb(sqlServerConnection, query)
+    fd = open('query/test.sql', 'r')
+    sqlFile = fd.read()
+    fd.close()
+
+    queryResult, columns = extractor.FromDb(sqlServerConnection, sqlFile)
 
     # Transform to dataframe
     transformer = t.Transform()
